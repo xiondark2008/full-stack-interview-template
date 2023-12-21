7112f285b3ec62233becdd5d5d628d6232a741a0
Justin Hansen <justin.hansen@improving.com>
2023-12-21 17:03:56 -0500

    Add movies endpoint to AppController

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,26 @@
 import { Controller, Get, Response } from '@nestjs/common';
 import { Response as expResponse } from 'express';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private moviesService = new AppService();
+  
   @Get('hello')
   getHello(@Response() res: expResponse): expResponse {
     return res.json('Hello World');
+  }
+
+  /**
+   * Get list of movies from provided source
+   * @param res 
+   * @returns 
+   */
+  @Get('movies')
+  async getMovies(@Response() res: expResponse): Promise<expResponse> {
+    const movies = await this.moviesService.getMovies();
+    const sortedMovies = this.moviesService.sortByTitle(movies);
+
+    return res.json(movies);
   }
 }

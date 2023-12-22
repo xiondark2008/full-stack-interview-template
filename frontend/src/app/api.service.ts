cb02bc6416d89799f1c81379933eacdba85e7a20
Justin Hansen <justin.hansen@improving.com>
2023-12-21 22:13:41 -0500

    Add getMovies method to ApiService

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -14,4 +14,10 @@ export class ApiService {
   getHelloWorld(): Observable<string> {
     return this.http.get<string>(`${this.baseUrl}/hello`);
   }
+
+  getMovies() {
+
+    //TODO: Replace this with a call to the backend
+    return [{"title":"A New Hope","director":"George Lucas","releaseDate":"1977-05-25"},{"title":"Attack of the Clones","director":"George Lucas","releaseDate":"2002-05-16"},{"title":"Return of the Jedi","director":"Richard Marquand","releaseDate":"1983-05-25"},{"title":"Revenge of the Sith","director":"George Lucas","releaseDate":"2005-05-19"},{"title":"The Empire Strikes Back","director":"Irvin Kershner","releaseDate":"1980-05-17"},{"title":"The Phantom Menace","director":"George Lucas","releaseDate":"1999-05-19"}];
+  }
 }
